9112fe1a67544e56407839c10b63c70707165c8a
SuprajaSivasubramanian <139612001+SuprajaSivasubramanian@users.noreply.github.com>
2024-07-12 13:18:08 +0530

    Add files via upload

---
+++ PRODIGY_WD_01/script.js
@@ -0,0 +1,15 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    const navbar = document.querySelector('.navbar');
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 50) {
+            document.body.classList.add('scrolled');
+        } else {
+            document.body.classList.remove('scrolled');
+        }
+    });
+});
+
+
